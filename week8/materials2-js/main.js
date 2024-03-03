8224bf3af03f8d3d7e6de6522b06e1c525951eda
Syahril Fitrawan Abadi <syahrilfitrawanabadi@gmail.com>
2024-03-03 21:22:36 +0800

    Add w8 materials2: javascript

---
+++ week8/materials2-js/main.js
@@ -0,0 +1,14 @@
+// Tipe Data
+// let x = 24;
+// const ini_constanta = "Ini string dari konstanta";
+
+// console.log(x);
+// console.log(ini_constanta);
+
+// Conditional
+let a = 9;
+if (a > 10) {
+    console.log(a);
+} else {
+    console.log("Tidak ada");
+}
