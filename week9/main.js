473db78dee67bd1bad013142f0d268a5e54b70cc
Syahril Fitrawan Abadi <syahrilfitrawanabadi@gmail.com>
2024-03-17 17:42:22 +0800

    Memasukkan week 9

---
+++ week9/main.js
@@ -0,0 +1,26 @@
+// let angka = parseInt(prompt("Masukkan: "));
+
+// if (angka > 30) {
+//     for (var i = angka; i <= 50; i++) {
+//         console.log(i);
+//     }
+// } else {
+//     for (var i = angka; i >= 0; i--) {
+//         console.log(i);
+//     }
+// }
+
+
+/* BREAK */
+// for(let i = 0; i < 5; i++){
+//     if(i == 3){
+//         break;
+//     }
+//     console.log("Ini adalah iterasi ke-", i + 1)
+// }
+
+for(let baris = 1; baris <= 2; baris++){
+    for(let kolom = 1; kolom <= 3; kolom++){
+        console.log("Ini adalah baris " + baris + ", kolom " + kolom);
+    }
+}
---
+++ week9/main2.js
@@ -0,0 +1,43 @@
+// MEMANGGIL HELLO
+// function sayHello(){
+//     console.log("Hello");
+// }
+// sayHello();
+
+// HITUNG PERSEGI
+// function luasPersegiPanjang(panjang, lebar){
+//     let luas = panjang * lebar;
+//     console.log(luas);
+// }
+
+// let luasPersegi = function(sisi){
+//     let luas = sisi * sisi;
+//     console.log(luas);
+// };
+
+// const luasSegitiga = (alas, tinggi) => {
+//     let luas = 1/2 * alas * tinggi;
+//     console.log(luas);
+// }
+
+// luasPersegiPanjang(5, 2);
+// luasPersegi(5);
+// luasSegitiga(10, 5);
+
+// LUAS PERSEGI
+// const luasLingkaran = (r) => 3.14 * r * r;
+// console.log(luasLingkaran(2));
+
+let name = ["Fitra", "Syahril", "Abadi"];
+
+const panggilNama = () => {
+    let pan = name.length;
+}
+
+// Panggil Nama
+let nama = "Syahril Fitrawan Abadi";
+let umur = 20;
+umur = String(umur); 
+console.log(umur);
+
+console.log(Math.round(Math.random() * 10));                                    
