9c90ae5a6af355593e9d8bec64bde20814a75854
Syahril Fitrawan Abadi <syahrilfitrawanabadi@gmail.com>
2024-03-30 23:13:35 +0800

    belajar object dan method JS

---
+++ week10/main.js
@@ -0,0 +1,45 @@
+// Object
+
+// const mobil = {
+//     warna : "Merah",
+//     jenisMesin : "Honda",
+//     kapasitasBensin: 200
+// };
+
+// console.log(mobil.warna)
+
+// // Method
+// const mobil = {
+//     gaskan (){
+//         console.log('Jalan Bro')
+//     },
+//     remBro(){
+//         console.log('Berhenti')
+//     }
+// }
+
+// mobil.gaskan();
+// mobil.remBro();
+
+// const airPlane = {
+//     crew: {
+//         captain: {
+//             name: "",
+//             age: 69
+//         },
+//         pramugara: {
+//             name: "Juan",
+//             age: 30
+//         },
+//         pramugari: {
+//             name: "Aul",
+//             age: 21
+//         }
+//     }
+// }
+
+let title = document.getElementById('judul')
+let tes = document.getElementsByClassName('tes')
+
+title.style.color = "red"
+tes[0].style.color = "green"
