d23f4c91a29ae9d4f1ee5e9954496eda1fe71769
Syahril Fitrawan Abadi <syahrilfitrawanabadi@gmail.com>
2024-03-30 23:11:01 +0800

    belajar event

---
+++ week11/index.js
@@ -0,0 +1,15 @@
+// const keydown = document.getElementById("keydown")
+// const pressed = document.getElementById("pressed")
+// const keyup = document.getElementById("keyup")
+
+// document.addEventListener("keydown", function(event){
+//     keydown.innerText = `menekan: ${event.key}`
+// })
+
+// document.addEventListener("keypress", function(event){
+//     keypressed.innerText = `menahan: ${event.key}`
+// })
+
+// document.addEventListener("keyup", function(event){
+//     keyup.innerText = `melepas: ${event.key}`
+// })
